97d11472b62517bf3e52dde53a0d4ca3b4885d5d
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-12 21:44:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  if array.length <= 3
+  if array.length < 3
     return array.join(" and ")
   else
     return array[0..(array.length - 2)].join(", ") + ", and #{array.last}"
